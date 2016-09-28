8a62649a7b5ff0f64f297495a3ed5150f84b41be
Orestis Meikopoulos <orme@ait.gr>
2016-09-28 21:00:19 +0300

    Final Commit

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public float autoLoadNextLevelAfter;
+	
+	void Start() {
+		if(autoLoadNextLevelAfter == 0) {
+			Debug.Log("Level auto load disabled");
+		} else {
+			Invoke("LoadNextLevel", autoLoadNextLevelAfter);
+		}
+	}
+
+	public void LoadLevel(string name) {
+		Debug.Log("Level load requested for " + name);
+		Application.LoadLevel(name);
+	}
+	
+	public void QuitRequest() {
+		Debug.Log("Quit game reguested!");
+		Application.Quit();
+	}
+	
+	public void LoadNextLevel() {
+		//catch the next level in the sequence in which we have put our levels in the build settings and load it automatically
+		Application.LoadLevel(Application.loadedLevel + 1);
+	}
+}
---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+
+	public AudioClip[] levelMusicChangeArray;
+	
+	private AudioSource audioSource;
+	
+	void Awake() {
+		DontDestroyOnLoad(gameObject);
+	}
+
+	// Use this for initialization
+	void Start () {
+		audioSource = GetComponent<AudioSource>();
+	}
+	
+	void OnLevelWasLoaded(int level) {
+		AudioClip thisLevelMusic = levelMusicChangeArray[level];
+		Debug.Log("Playing clip: " + thisLevelMusic);
+		
+		if(thisLevelMusic) {
+			audioSource.clip = thisLevelMusic;
+			audioSource.loop = true;
+			audioSource.Play();
+		}
+	}
+}
